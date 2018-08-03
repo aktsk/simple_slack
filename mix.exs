6495298cbdeb003be85be2b2133a5ff1992b6435
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2018-08-04 01:59:37 +0900

    Update Elixir and libraries

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleSlack.Mixfile do
   def project do
     [app: :simple_slack,
      version: "1.0.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.7.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -18,10 +18,10 @@ defmodule SimpleSlack.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, ">= 0.9.0"},
-     {:poison, ">= 2.2.0"},
-     {:credo, ">= 0.4.8", only: :dev},
-     {:ex_doc, ">= 0.13.0", only: :dev}]
+    [{:httpoison, "~> 1.2.0"},
+     {:poison, "~> 4.0.1"},
+     {:credo, ">= 0.10.0", runtime: false, only: :dev},
+     {:ex_doc, ">= 0.19.0", runtime: false, only: :dev}]
   end
 
   defp package do
